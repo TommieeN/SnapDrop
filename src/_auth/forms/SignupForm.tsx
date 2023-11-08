d3e9dd123a6f4bbe1471cae425f024fbac4165ce
TommyNguyen <timsnguyen@hotmail.com>
2023-11-07 18:17:40 -0800

    appwrite auth setup

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -32,9 +32,8 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // create the user
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    // const newUser = await createUserAccount(values);
   }
   return (
     <Form {...form}>
