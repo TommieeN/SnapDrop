ee22a5e473900a80e53d284a5f27694990190c74
TommyNguyen <timsnguyen@hotmail.com>
2023-11-10 17:10:27 -0800

    format date function

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -7,6 +7,7 @@ import {
 import {
   createPost,
   createUserAccount,
+  getRecentPosts,
   signInAccount,
   signOutAccount,
 } from "../appwrite/api";
@@ -25,6 +26,7 @@ export const useSignInAccount = () => {
       signInAccount(user),
   });
 };
+
 export const useSignOutAccount = () => {
   return useMutation({
     mutationFn: signOutAccount,
@@ -43,3 +45,10 @@ export const useCreatePost = () => {
     },
   });
 };
+
+export const useGetRecentPosts = () => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+    queryFn: getRecentPosts,
+  })
+}
