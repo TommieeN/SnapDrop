3265e19356c84d19ebf4b0c713eae178715abb70
TommyNguyen <timsnguyen@hotmail.com>
2023-11-17 22:29:06 -0800

    finish liked posts

--- src/components/shared/GridPostList.tsx
+++ src/components/shared/GridPostList.tsx
@@ -28,14 +28,14 @@ const GridPostList = ({
           </Link>
           <div className="grid-post_user">
             {showUser && (
-              <div className="flex items-center justify-start gap-2 flex-1">
+              <Link to={`/profile/${post.creator.$id}`} className="flex items-center justify-start gap-2 flex-1">
                 <img
                   src={post.creator.imageUrl}
                   alt="creator"
                   className="h-8 w-8 rounded-full"
                 />
                 <p className="line-clamp-1">{post.creator.name}</p>
-              </div>
+              </Link>
             )}
             {showStats && <PostStats post={post} userId={user.id} />}
           </div>
