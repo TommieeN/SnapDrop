827565a1b8af215a195c7753b3fa8720d1272c6c
TommyNguyen <timsnguyen@hotmail.com>
2023-11-22 22:27:28 -0800

    fix button styling for edit profile form

--- src/components/forms/EditProfileForm.tsx
+++ src/components/forms/EditProfileForm.tsx
@@ -1,8 +1,8 @@
-import * as z from "zod"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { ProfileValidation } from "@/lib/validation"
-import { Button } from "@/components/ui/button"
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { ProfileValidation } from "@/lib/validation";
+import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
@@ -11,42 +11,43 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import { useUserContext } from "@/context/AuthContext"
-import { Textarea } from "../ui/textarea"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useUserContext } from "@/context/AuthContext";
+import { Textarea } from "../ui/textarea";
 
 const EditProfileForm = () => {
-    const { user, setUser } = useUserContext()
-    const form = useForm<z.infer<typeof ProfileValidation>>({
-        resolver: zodResolver(ProfileValidation),
-        defaultValues: {
-          file: [],
-          name: user.name,
-          username: user.username,
-          email: user.email,
-          bio: user.bio || "",
-        },
-      })
-     
-      // 2. Define a submit handler.
-      function onSubmit(values: z.infer<typeof ProfileValidation>) {
-        // Do something with the form values.
-        // ✅ This will be type-safe and validated.
-        console.log(values)
-      }
-    
+  const { user, setUser } = useUserContext();
+  const form = useForm<z.infer<typeof ProfileValidation>>({
+    resolver: zodResolver(ProfileValidation),
+    defaultValues: {
+      file: [],
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      bio: user.bio || "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof ProfileValidation>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
     <Form {...form}>
-      <form className="flex flex-col gap-7 w-full mt-4 max-w-5xl" onSubmit={form.handleSubmit(onSubmit)}>
-      <FormField
+      <form
+        className="flex flex-col gap-7 w-full mt-4 max-w-5xl"
+        onSubmit={form.handleSubmit(onSubmit)}
+      >
+        <FormField
           control={form.control}
           name="file"
           render={({ field }) => (
             <FormItem className="flex">
-              <FormControl>
-                {/* <ProfileUploader /> */}
-              </FormControl>
+              <FormControl>{/* <ProfileUploader /> */}</FormControl>
               <FormMessage className="shad-form_message" />
             </FormItem>
           )}
@@ -84,7 +85,7 @@ const EditProfileForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label">email</FormLabel>
               <FormControl>
-                <Input className="shad-input" type="text" {...field} disabled/>
+                <Input className="shad-input" type="text" {...field} disabled />
               </FormControl>
               <FormMessage />
             </FormItem>
@@ -97,19 +98,29 @@ const EditProfileForm = () => {
             <FormItem>
               <FormLabel className="shad-form_label">Bio</FormLabel>
               <FormControl>
-                <Textarea className="shad-textarea custom-scrollbar"
-                {...field}
+                <Textarea
+                  className="shad-textarea custom-scrollbar"
+                  {...field}
                 />
               </FormControl>
-              <FormMessage className="shad-form_message"/>
+              <FormMessage className="shad-form_message" />
             </FormItem>
           )}
         />
-        <Button type="button">cancel</Button>
-        <Button type="submit">Submit</Button>
+        <div className="flex gap-4 items-center justify-end">
+          <Button className="shad-button_dark_4" type="button">
+            Cancel
+          </Button>
+          <Button
+            className="shad-button_primary whitespace-nowrap"
+            type="submit"
+          >
+            Update Profile
+          </Button>
+        </div>
       </form>
     </Form>
-  )
-}
+  );
+};
 
-export default EditProfileForm
+export default EditProfileForm;
