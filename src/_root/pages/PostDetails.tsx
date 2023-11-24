455ea0573c44fa763d7e0331af9164df1deb1617
TommyNguyen <timsnguyen@hotmail.com>
2023-11-24 12:43:33 -0800

    fixed delete post button on post details

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -23,7 +23,6 @@ const PostDetails = () => {
             alt="creator"
             className="post_details-img"
           />
-
           <div className="post_details-info">
             <div className="flex-between w-full">
               <Link
@@ -38,7 +37,6 @@ const PostDetails = () => {
                   alt="creator"
                   className="rounded-full w-8 h-8 lg:w-12 lg:h-12"
                 />
-
                 <div className="flex flex-col">
                   <p className="base-medium lg:body-bold text-light-1">
                     {post?.creator.name}
@@ -66,11 +64,10 @@ const PostDetails = () => {
                     alt="edit"
                   />
                 </Link>
-
                 <Button
                   onClick={handleDeletePost}
                   variant="ghost"
-                  className={`ghost_details-delete_btn${
+                  className={`ghost_details-delete_btn ${
                     user.id !== post?.creator.$id && "hidden"
                   }`}
                 >
@@ -83,7 +80,6 @@ const PostDetails = () => {
                 </Button>
               </div>
             </div>
-
             <hr className="border w-full border-dark-4/80" />
             <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
               <p>{post?.caption}</p>
@@ -95,9 +91,8 @@ const PostDetails = () => {
                 ))}
               </ul>
             </div>
-
             <div className="w-full">
-                  <PostStats post={post} userId={user.id}/>
+              <PostStats post={post} userId={user.id} />
             </div>
           </div>
         </div>
